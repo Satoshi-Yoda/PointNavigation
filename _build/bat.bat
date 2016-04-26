fa7b4dceca929a4b0a3d7e0541e27bd43498f756
Satoshi-Yoda <Satoshi.Yoda.Register.264@gmail.com>
2016-04-26 19:33:36 +0300

    Build it.

---
+++ _build/bat.bat
@@ -0,0 +1,7 @@
+copy "C:\Program Files\LOVE\DevIL.dll" DevIL.dll
+copy "C:\Program Files\LOVE\love.dll" love.dll
+copy "C:\Program Files\LOVE\lua51.dll" lua51.dll
+copy "C:\Program Files\LOVE\mpg123.dll" mpg123.dll
+copy "C:\Program Files\LOVE\OpenAL32.dll" OpenAL32.dll
+copy "C:\Program Files\LOVE\SDL2.dll" SDL2.dll
+copy /b "C:\Program Files\LOVE\love.exe"+love.love exe.exe
